bd7d84e2da343725294c34451ff994ef146d870c
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-11 07:44:39 +0700

    Update 1_create_table.sql

--- 1_create_table.sql
+++ 1_create_table.sql
@@ -1,4 +1,4 @@
-
+--Write a SQL statement to create a simple table countries including columns country_id,country_name and region_id.--
 CREATE TABLE countries (
     country_id CHAR(2) PRIMARY KEY,
     country_name VARCHAR(50) NOT NULL,
