e4a9e3e8ad4da1aa18cde70351022e5dcc208e9f
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-11 08:27:36 +0700

    Update 1_create_table.sql

--- 1_create_table.sql
+++ 1_create_table.sql
@@ -204,6 +204,15 @@ CREATE TABLE employees (
     PRIMARY KEY (employee_id),
     FOREIGN KEY (job_id) REFERENCES jobs(JOB_ID) ON UPDATE RESTRICT ON DELETE CASCADE
 );
+--Write a SQL statement to create a table employees including columns
+--employee_id--
+--first_name--
+--last_name--
+--job_id--
+--salary--
+--make sure that, the employee_id column does not contain any duplicate value at the time of insertion
+--the foreign key column job_id, referenced by the column job_id of jobs table, can contain only those values which are exists in the jobs table.
+--The specialty of the statement is that, The ON DELETE SET NULL action will set the foreign key column values in the child table(employees) to NULL when the record in the parent table(jobs) is deleted, with a condition that the foreign key column in the child table must accept NULL values and the ON UPDATE SET NULL action resets the values in the rows in the child table(employees) to NULL values when the rows in the parent table(jobs) are updated.
 CREATE TABLE employees (
     employee_id INT NOT NULL,
     first_name VARCHAR(50),
