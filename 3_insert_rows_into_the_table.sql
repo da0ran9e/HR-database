0a3f200406765239e6a37df5ace41eda61eadfd1
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-29 22:01:35 +0700

    Create 3_insert_rows_into_the_table.sql

---
+++ 3_insert_rows_into_the_table.sql
@@ -0,0 +1,44 @@
+-- 1. Insert a record with your own values into the table countries
+INSERT INTO countries (COUNTRY_ID, COUNTRY_NAME, REGION_ID) VALUES ('XX', 'Your Country', 9999);
+
+-- 2. Insert one row into the table countries against the columns country_id and country_name
+INSERT INTO countries (COUNTRY_ID, COUNTRY_NAME) VALUES ('XY', 'New Country');
+
+-- 3. Create a duplicate of the countries table named country_new with all structure and data
+CREATE TABLE country_new AS SELECT * FROM countries;
+
+-- 4. Insert NULL values against the region_id column for a row in the countries table
+INSERT INTO countries (COUNTRY_ID, COUNTRY_NAME, REGION_ID) VALUES ('ZZ', 'No Region', NULL);
+
+-- 5. Insert 3 rows using a single INSERT statement into the countries table
+INSERT INTO countries (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+VALUES
+    ('AA', 'Country A', 1001),
+    ('BB', 'Country B', 1002),
+    ('CC', 'Country C', 1003);
+
+-- 6. Insert rows from the country_new table into the countries table
+INSERT INTO countries (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+SELECT COUNTRY_ID, COUNTRY_NAME, REGION_ID
+FROM country_new;
+
+-- 7. Insert one row in the jobs table to ensure that no duplicate value will be entered in the job_id column
+INSERT INTO jobs (job_id, job_title, min_salary, max_salary) VALUES ('JOBX', 'Job Title X', 5000, 10000);
+
+-- 10. Insert rows into the countries table where the country_id column will be unique and auto-incremented
+INSERT INTO countries (COUNTRY_NAME, REGION_ID) VALUES ('Country 1', 1001), ('Country 2', 1002), ('Country 3', 1003);
+
+-- 11. Insert records into the countries table to ensure that the country_id column will not contain any duplicate data, automatically incremented, and the column country_name will be filled with 'N/A' if no value is assigned
+INSERT INTO countries (COUNTRY_NAME) VALUES ('Country A'), (NULL), ('Country B');
+
+-- 12. Insert rows into the job_history table where the job_id column contains values that exist in the job_id column of the jobs table
+INSERT INTO job_history (employee_id, start_date, end_date, job_id, department_id)
+VALUES (1, '2023-01-01', '2023-12-31', 'JOBX', 1001);
+
+-- 13. Insert rows into the employees table where the combination of department_id and manager_id contains a unique value and the combined values must exist in the departments table
+INSERT INTO employees (employee_id, first_name, last_name, department_id, manager_id, salary)
+VALUES (1, 'John', 'Doe', 1001, 1001, 5000), (2, 'Jane', 'Smith', 1002, 1002, 6000);
+
+-- 14. Insert rows into the employees table where the combination of department_id and job_id contains values that must exist in the departments and jobs tables
+INSERT INTO employees (employee_id, first_name, last_name, department_id, job_id, salary)
+VALUES (1, 'John', 'Doe', 1001, 'JOBX', 5000), (2, 'Jane', 'Smith', 1002, 'JOBY', 6000);
